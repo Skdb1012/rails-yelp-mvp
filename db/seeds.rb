179a5e6baa09803b58e485bbedd0f913d94df5d4
STEPHANE DEBUICHE <debuiche.stephane@gmail.com>
2022-07-19 19:13:40 +0100

    5 restos seede

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create({name: 'Le Fouquets', address: 'Paris', phone_number: '0123357811', category: 'french' })
+Restaurant.create({name: 'Les délices de Pékin', address: 'Courbevoie', phone_number: '0178649087', category: 'chinese' })
+Restaurant.create({name: 'Il Giardino', address: 'Neuilly sur Seine', phone_number: '0144876589', category: 'italian' })
+Restaurant.create({name: 'Chez Didier', address: 'Lieges', phone_number: '0476890086', category: 'belgian' })
+Restaurant.create({name: 'Le monde du sushi', address: 'Lille', phone_number: '0156097784', category: 'japanese' })
